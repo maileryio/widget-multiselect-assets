99ac75ace34f05aaef56f86c99dfbd73f922c8f9
Igor Maliy <imaliy.filsh@gmail.com>
2022-04-18 10:04:23 +0300

    Development

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
-    <v-select taggable multiple :options="['Canada', 'United States']"></v-select>
+    <v-select
+      v-model="selected"
+      :multiple="multiple"
+      :taggable="taggable"
+      :options="options"
+      :reduce="(option) => option.id">
+    </v-select>
   </div>
 </template>
 
 <script>
-  import vSelect from 'vue-select'
-
-  Vue.component('v-select', vSelect)
+  import vSelect from 'vue-select';
+  import map from 'lodash/map';
 
   export default {
     name: 'ui-select',
     components: {
       vSelect,
+    },
+    props: {
+      multiple: Boolean,
+      taggable: Boolean,
+      items: Object,
+      value: [Array, String]
+    },
+    computed: {
+      options: function () {
+        return map(JSON.parse(this.items), (label, id) => {
+          return {id, label};
+        });
+      },
+      selected: function () {
+        return JSON.parse(this.value);
+      }
     }
   }
 </script>
