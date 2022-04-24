a17f3e82456d984c9109c26637c311813d324f5d
Igor Maliy <imaliy.filsh@gmail.com>
2022-04-24 22:29:55 +0300

    Development

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,22 +1,29 @@
 <template>
-  <div>
-    <div v-if="selected && this.multiple">
+  <div class="ui-select">
+    <div v-if="hasSelected && multiple">
       <input v-for="value in selected" type="hidden" :name="name + '[]'" :value="value" />
     </div>
-    <div v-else-if="selected">
+    <div v-else-if="hasSelected">
       <input type="hidden" :name="name" :value="selected" />
     </div>
 
     <v-select
       v-model="selected"
+      :class="className"
       :multiple="multiple"
       :taggable="taggable"
+      :clearable="clearable"
+      :searchable="searchable"
+      :deselectFromDropdown="deselectFromDropdown"
+      :closeOnSelect="closeOnSelect"
       :options="options"
-      :reduce="(option) => option.id">
+      :placeholder="placeholder"
+      :reduce="(option) => option.id"
+      :selectable="(option) => !!option.id">
 
       <template #search="{attributes, events}">
         <input
-          class="vs__search form-control"
+          class="vs__search"
           :required="required"
           v-bind="attributes"
           v-on="events"
@@ -29,6 +36,7 @@
 
 <script>
   import vSelect from 'vue-select';
+  import filter from 'lodash/filter';
   import map from 'lodash/map';
 
   export default {
@@ -38,23 +46,60 @@
     },
     props: {
       name: String,
-      multiple: Boolean,
-      taggable: Boolean,
-      required: Boolean,
+      multiple: {
+      	type: Boolean,
+      	default: false
+      },
+      taggable: {
+      	type: Boolean,
+      	default: false
+      },
+      searchable: {
+      	type: Boolean,
+      	default: true
+      },
+      clearable: {
+      	type: Boolean,
+      	default: true
+      },
+      deselectFromDropdown: {
+          type: Boolean,
+          default: false
+      },
+      closeOnSelect: {
+      	type: Boolean,
+      	default: true
+      },
+      required: {
+      	type: Boolean,
+      	default: false
+      },
       items: Object,
-      value: [Array, String]
+      value: [Array, String],
+      className: String,
+      placeholder: String
     },
-    data() {console.log(this.required)
+    data() {
       return {
+        selected: (() => {
+          const value = this.value ? JSON.parse(this.value) : null;
+
+          if (!this.multiple && Array.isArray(value)) {
+            return value[0];
+          }
+          return value;
+        })(),
         options: map(JSON.parse(this.items), (label, id) => {
           return {id, label};
-        }),
-        selected: (() => {
-          if (this.multiple && this.value) {
-            return JSON.parse(this.value);
-          }
-          return this.value;
-        })()
+        })
+      }
+    },
+    computed: {
+      hasSelected () {
+        return this.selected && this.selected.length > 0;
+      },
+      required () {
+        return this.$props.required && this.selected && this.selected.length == 0;
       }
     }
   }
@@ -62,20 +107,28 @@
 
 <style src="vue-select/dist/vue-select.css"></style>
 
-<style>
+<style lang="scss">
   * {
     --vs-dropdown-bg: #fff;
   }
 
-  .v-select {
-    background-color: var(--vs-dropdown-bg);
-  }
+  .ui-select {
+    .v-select {
+      background-color: var(--vs-dropdown-bg);
+      height: auto;
+    }
 
-  .vs__search {
-    margin: 0;
-  }
+    .vs__search {
+      margin: 0;
+    }
 
-  .vs__dropdown-toggle {
-    padding: 0;
+    .vs__dropdown-toggle {
+      border: none;
+      padding: 0 0 2px;
+    }
+
+    .vs__selected {
+      margin: 2px 2px 0;
+    }
   }
 </style>
